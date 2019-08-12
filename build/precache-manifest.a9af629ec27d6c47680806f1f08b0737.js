487012a1beb4659b4b76b5e4c69cd42cd14a6033
Felipe Cavalcante <felipe.cavalcante.rod@gmail.com>
2019-08-12 18:26:13 -0300

    pasta build com a ultima versão

---
+++ build/precache-manifest.a9af629ec27d6c47680806f1f08b0737.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa32b35874bdbeabb0296aaf57d52963",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a98e6de3b9855f5a9bb",
+    "url": "/static/css/2.a0ecda1b.chunk.css"
+  },
+  {
+    "revision": "2efed6bfccc2c2ba99d0",
+    "url": "/static/css/main.8f675491.chunk.css"
+  },
+  {
+    "revision": "8a98e6de3b9855f5a9bb",
+    "url": "/static/js/2.48171884.chunk.js"
+  },
+  {
+    "revision": "2efed6bfccc2c2ba99d0",
+    "url": "/static/js/main.f466a9cc.chunk.js"
+  },
+  {
+    "revision": "a94bfa2a07fad8bb364b",
+    "url": "/static/js/runtime~main.a3819360.js"
+  }
+]);
